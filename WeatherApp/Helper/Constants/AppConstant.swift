45e4da03716dda130d5b3459b85935da192f6a38
Suraj Kumar Mandal <surajkumarmandal1@gmail.com>
2022-06-16 11:37:00 +0530

    Update AppConstant.swift

--- WeatherApp/Helper/Constants/AppConstant.swift
+++ WeatherApp/Helper/Constants/AppConstant.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct Constant {
     
-    static let GOOGLE_PLACE_API_KEY = "AIzaSyCzSS8SAZbxym9ZczTs9b4P0p9oB9A8Sik"
-    static let OPEN_WEATHERMAP_API = "d150841691f8332137a99035e60907ed"
+    static let GOOGLE_PLACE_API_KEY = ""
+    static let OPEN_WEATHERMAP_API = ""
     
     static let internetAlertTitle      = "No Internet"
     static let internetAlertMessage    = "Internet connection is not available, please check your internet connection and try again"
